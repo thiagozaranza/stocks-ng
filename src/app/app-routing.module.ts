7fa619af4b727d9ead6352881f69226470abb05c
Thiago Zaranza <thiagozaranza@gmail.com>
2019-02-23 23:10:36 -0300

    First modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RootComponent } from './layout/root/root.component';
+import { EmpresaIndexComponent } from './empresa/empresa-index/empresa-index.component';
+import { EmpresaCreateComponent } from './empresa/empresa-create/empresa-create.component';
+import { SetorIndexComponent } from './setor/setor-index/setor-index.component';
+import { SetorCreateComponent } from './setor/setor-create/setor-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RootComponent,
+    children:  [
+      { 
+        path: '', 
+        redirectTo: 'setor', 
+        pathMatch: 'full' 
+      },
+      {
+        path: 'empresa',
+        component: EmpresaIndexComponent
+      }, {
+        path: 'empresa/create',
+        component: EmpresaCreateComponent
+      }, {
+        path: 'setor',
+        component: SetorIndexComponent
+      }, {
+        path: 'setor/create',
+        component: SetorCreateComponent
+      }
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpresaModule } from './empresa/empresa.module';
+import { SetorModule } from './setor/setor.module';
+import { SubsetorModule } from './subsetor/subsetor.module';
+import { SegmentoModule } from './segmento/segmento.module';
+import { LayoutModule } from './layout/layout.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    EmpresaModule,
+    SetorModule,
+    SubsetorModule,
+    SegmentoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
